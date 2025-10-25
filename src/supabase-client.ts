a7753eaed94a9e991064a5f83ae4c822904986ed
Charlesdeo Bansi <cbansi.student@asiancollege.edu.ph>
2025-10-25 19:33:44 +0800

    Initial commit

---
+++ src/supabase-client.ts
@@ -0,0 +1,5 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient("https://vkjkljbjcesvudnrfaey.supabase.co", 
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZramtsamJqY2VzdnVkbnJmYWV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAxNTIzMDgsImV4cCI6MjA3NTcyODMwOH0.CurKvBjj_ZKrPiTy0XuAMF2OajOeswRGwfF1_04T9ug"
+);
